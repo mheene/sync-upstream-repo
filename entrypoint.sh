f81dbc28d8b302e496b42e7b83a83e9d2bacb777
Markus Heene <markus.heene@gmail.com>
2019-12-22 18:51:43 +0100

    entrypoint.sh changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ echo "UPSTREAM_REPO=$UPSTREAM_REPO"
 
 git clone "https://github.com/${GITHUB_REPOSITORY}.git"
 
-cd `echo ${GITHUB_REPOSITORY} | cut -f 2--delimiter="/" `
+cd `echo ${GITHUB_REPOSITORY} | cut -f 2 --delimiter="/" `
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
