a4d2b3d4a4c794285f4886486fa1b6aa6d2454e8
Piggy NL <piggynl@outlook.com>
2022-05-26 09:07:20 +0800

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ fi
 echo "UPSTREAM_REPO=$UPSTREAM_REPO"
 
 git clone "https://github.com/${GITHUB_REPOSITORY}.git" work
-cd work || { echo "Missing work dir" && exist 2 ; }
+cd work || { echo "Missing work dir" && exit 2 ; }
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
